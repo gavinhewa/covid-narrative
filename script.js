17ac42c52983d0ddfbcfd4a452afd5c265805d0e
Gavin Hewawasam <gavinh4@illinois.edu>
2026-08-02 21:00:19 -0400

    add bar charts for scenes 2 and 3

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let covidData;
 let usData;
 let latestData = {};
 let colorScale;
+let top10Cases = [];
+let top10Deaths = [];
 
 const svg = d3.select("#chart");
 let currentScene = "overview";
@@ -15,7 +17,7 @@ const tooltip = d3.select("body")
     .style("padding", "10px")
     .style("border", "1px solid black")
     .style("border-radius", "4px")
-    .style("pointer-events", "none"); // Prevents tooltip from interfering with mouse events
+    .style("pointer-events", "none");
 
 // 1. Load data asynchronously
 Promise.all([
@@ -39,14 +41,15 @@ Promise.all([
         .domain([0, maxCases])
         .interpolator(d3.interpolateReds);
 
-    const states = Object.entries(latestData).map(([state, values]) => ({
+    const statesArray = Object.entries(latestData).map(([state, values]) => ({
         state,
         cases: values.cases,
         deaths: values.deaths
     }));
 
-    states.sort((a, b) => b.cases - a.cases);
-    const top10 = states.slice(0, 10);
+    // Generate Top 10 Lists for Cases and Deaths
+    top10Cases = [...statesArray].sort((a, b) => b.cases - a.cases).slice(0, 10);
+    top10Deaths = [...statesArray].sort((a, b) => b.deaths - a.deaths).slice(0, 10);
 
     // 4. Initial render once everything is ready
     renderScene();
@@ -106,19 +109,119 @@ function drawOverview() {
 }
 
 function drawCases() {
+    const margin = {top: 60, right: 40, bottom: 40, left: 120};
+    const width = 1000 - margin.left - margin.right;
+    const height = 650 - margin.top - margin.bottom;
+
+    const chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Title
     svg.append("text")
-        .attr("x", 350)
-        .attr("y", 100)
-        .attr("font-size", 30)
-        .text("Cases Scene");
+        .attr("x", margin.left)
+        .attr("y", 35)
+        .attr("font-size", 22)
+        .attr("font-weight", "bold")
+        .text("Top 10 States by COVID-19 Cases");
+
+    const yScale = d3.scaleBand()
+        .domain(top10Cases.map(d => d.state))
+        .range([0, height])
+        .padding(0.1);
+
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(top10Cases, d => d.cases)])
+        .range([0, width]);
+
+    // Draw Bars
+    chartGroup.selectAll("rect")
+        .data(top10Cases)
+        .enter()
+        .append("rect")
+        .attr("y", d => yScale(d.state))
+        .attr("x", 0)
+        .attr("height", yScale.bandwidth())
+        .attr("width", d => xScale(d.cases))
+        .attr("fill", "#d95f0e")
+        .on("mouseover", function(event, d) {
+            tooltip
+                .style("visibility", "visible")
+                .html(`<strong>${d.state}</strong><br>Cases: ${d.cases.toLocaleString()}`);
+        })
+        .on("mousemove", function(event) {
+            tooltip
+                .style("top", (event.pageY - 10) + "px")
+                .style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function() {
+            tooltip.style("visibility", "hidden");
+        });
+
+    // Axes
+    chartGroup.append("g")
+        .call(d3.axisLeft(yScale));
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale).ticks(5));
 }
 
 function drawDeaths() {
+    const margin = {top: 60, right: 40, bottom: 40, left: 120};
+    const width = 1000 - margin.left - margin.right;
+    const height = 650 - margin.top - margin.bottom;
+
+    const chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Title
     svg.append("text")
-        .attr("x", 350)
-        .attr("y", 100)
-        .attr("font-size", 30)
-        .text("Deaths Scene");
+        .attr("x", margin.left)
+        .attr("y", 35)
+        .attr("font-size", 22)
+        .attr("font-weight", "bold")
+        .text("Top 10 States by COVID-19 Deaths");
+
+    const yScale = d3.scaleBand()
+        .domain(top10Deaths.map(d => d.state))
+        .range([0, height])
+        .padding(0.1);
+
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(top10Deaths, d => d.deaths)])
+        .range([0, width]);
+
+    // Draw Bars
+    chartGroup.selectAll("rect")
+        .data(top10Deaths)
+        .enter()
+        .append("rect")
+        .attr("y", d => yScale(d.state))
+        .attr("x", 0)
+        .attr("height", yScale.bandwidth())
+        .attr("width", d => xScale(d.deaths))
+        .attr("fill", "#ae017e")
+        .on("mouseover", function(event, d) {
+            tooltip
+                .style("visibility", "visible")
+                .html(`<strong>${d.state}</strong><br>Deaths: ${d.deaths.toLocaleString()}`);
+        })
+        .on("mousemove", function(event) {
+            tooltip
+                .style("top", (event.pageY - 10) + "px")
+                .style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function() {
+            tooltip.style("visibility", "hidden");
+        });
+
+    // Axes
+    chartGroup.append("g")
+        .call(d3.axisLeft(yScale));
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale).ticks(5));
 }
 
 // Event Listeners for Navigation Buttons
